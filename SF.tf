07a0bf6dc7c2f1d00b71d2f76b2e350dca3b9a5f
awspushpkant <71886805+awspushpkant@users.noreply.github.com>
2020-09-25 21:52:06 +0530

    Update SF.tf

--- SF.tf
+++ SF.tf
@@ -2,9 +2,7 @@ provider "aws" {
   region     = "ap-south-1"
   }
 
-
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
+resource "aws_instance" "myec2" {
+   ami = "ami-0e306788ff2473ccb"
+   instance_type = "t2.micro"
 }
