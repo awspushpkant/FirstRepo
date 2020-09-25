644ac6f80ad4eb9c6bd2d4c48000964db03f5b93
awspushpkant <71886805+awspushpkant@users.noreply.github.com>
2020-09-25 19:29:19 +0530

    Update SF.tf

--- SF.tf
+++ SF.tf
@@ -8,8 +8,3 @@ resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
 }
-resource "aws_security_group" "allow_tls1" {
-  provider = aws.us
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-}
