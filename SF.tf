32feffa6a189c24e80bd556c63f2dc9474894650
awspushpkant <71886805+awspushpkant@users.noreply.github.com>
2020-09-25 19:14:42 +0530

    SG Creation for Terraform Cloud

---
+++ SF.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+}
+resource "aws_security_group" "allow_tls1" {
+  provider = aws.us
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+}
